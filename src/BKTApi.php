b44cb8403d7f4bbdbb096f3b94dd18ed6b1ed38c
Booklet <mikolajczakbartosz@gmail.com>
2017-01-31 14:45:26 +0100

    Add support to PUT, DELETE request by POST request

--- src/BKTApi.php
+++ src/BKTApi.php
@@ -19,10 +19,27 @@ class BKTApi
         return $api->request('PUT', $resource, $data);
     }
 
+    public static function putByPost($resource, $data)
+    {
+        if (!isset($data['_method'])) {
+            $data = ['_method' => 'PUT'];
+        }
+        $api = new self;
+        return $api->request('POST', $resource, $data);
+    }
+
     public static function delete($resource)
     {
         $api = new self;
         return $api->request('DELETE', $resource);
+    }
+
+    public static function deleteByPost($resource)
+    {
+        $data = [];
+        $data = ['_method' => 'DELETE'];
+        $api = new self;
+        return $api->request('POST', $resource, $data);
     }
 
     private function request($method, $resource, $data = null, $options = [])
